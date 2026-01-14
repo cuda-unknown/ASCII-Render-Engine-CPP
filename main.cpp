025e1bb9ce1f95abb47c70fa94345259101b9ecc
cuda-unknown <soonyouwillknow34@gmail.com>
2026-01-14 12:51:47 +0530

    Feat: Implement in-place matrix rotation for ASCII buffer

--- main.cpp
+++ main.cpp
@@ -2,28 +2,58 @@
 #include<vector>
 #include<cmath>
 using namespace std;
-void rendercircle(int width,int height,float radius ){
-      float centerX=width/2.0;
-      float centerY=height/2.0;
+char screen[40][40]; 
 
-      for(int y=0;y<height;y++){
-        for(int x=0;x<width;x++){
-            float dx=x-centerX;
-            float dy=y-centerY;
-            float distance=sqrt(dx*dx+dy*dy);
+void rotateframe(int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = i + 1; j < size; j++) {
+            swap(screen[i][j], screen[j][i]);
+        }
+    }
+    for(int i = 0; i < size; i++) {
+        int start = 0;
+        int end = size - 1;
+        while(start < end) {
+            swap(screen[i][start], screen[i][end]);
+            start++;
+            end--;
+        }
+    }
+}
+void rendercircle(int size, float radius) {
+    float centerX = size / 2.0;
+    float centerY = size / 2.0;
 
-            if(distance<radius){
-                cout<<"#";
-            }else{
-                cout<<" ";
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            screen[i][j] = ' ';
+        }
+    }
+
+    for(int y = 0; y < size; y++) {
+        for(int x = 0; x < size; x++) {
+            float dx = x - centerX;
+            float dy = y - centerY;
+
+            float distance = sqrt(dx*dx + dy*dy);
+
+            if(distance < radius) {
+                screen[y][x] = '#';
             }
         }
-        cout<<endl;
+    }
+
+    rotateframe(size);
+    
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            cout << screen[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 int main() {
-    int width=40;
-    int height=20;
+    int size=30;
     string shape;
     cout << "What shape do you want? (circle/square): ";
     cin >> shape;
@@ -32,7 +62,7 @@ int main() {
         float r;
         cout << "Enter radius: ";
         cin >> r;
-        rendercircle(40, 20, r);
+        rendercircle(size, r);
     } else {
         cout << "Shape not supported yet!" << endl;
     }
